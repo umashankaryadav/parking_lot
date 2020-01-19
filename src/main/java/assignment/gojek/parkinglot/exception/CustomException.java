0ec28b8c290bacab516ae9f79b582b4aaed8cf65
Uma Shankar <uma.shankar@practo.com>
2020-01-19 16:53:06 +0530

    custom exception

---
+++ src/main/java/assignment/gojek/parkinglot/exception/CustomException.java
@@ -0,0 +1,9 @@
+package assignment.gojek.parkinglot.exception;
+
+public class CustomException extends RuntimeException {
+
+  public CustomException(String message)
+  {
+    super(message);
+  }
+}
